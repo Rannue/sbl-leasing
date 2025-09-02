bc59626b10637f478c6bc11e820a105bac438f15
Anna B <yavianbo@gmail.com>
2025-09-02 12:30:09 +0300

    feat: FAQ toggle icons - add plus/minus icon switching

--- pages/faq/faq.js
+++ pages/faq/faq.js
@@ -2,11 +2,22 @@ const FAQInfoBlock = document.querySelectorAll(".faq__question");
 FAQInfoBlock.forEach((item) => {
   item.addEventListener("click", () => {
     const answer = item.nextElementSibling;
+    const plusIcon = item.querySelector(".faq__icon-plus");
+    const minusIcon = item.querySelector(".faq__icon-minus");
+
     console.log(answer);
     if (answer.classList.contains("open")) {
+      // Закрываем ответ
       answer.classList.remove("open");
+      // Показываем плюс, скрываем минус
+      plusIcon.style.display = "block";
+      minusIcon.style.display = "none";
     } else {
+      // Открываем ответ
       answer.classList.add("open");
+      // Скрываем плюс, показываем минус
+      plusIcon.style.display = "none";
+      minusIcon.style.display = "block";
     }
   });
 });
